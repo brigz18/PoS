7f2ef8d0cfe47083e864b1f0b95d2008cb44971d
brigz18 <bringinoroiandrey@mqcqc.ph.education>
2026-07-29 13:37:11 +0800

    Delete asyncHandler.js

--- asyncHandler.js
+++
@@ -1,5 +0,0 @@
-// Wraps async route handlers so thrown errors are forwarded to Express error middleware
-const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = asyncHandler;
