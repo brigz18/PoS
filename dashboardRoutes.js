0b11f2670eca6fd8156c5e7156f606f090e91cbd
brigz18 <bringinoroiandrey@mqcqc.ph.education>
2026-08-16 01:12:41 +0800

    Delete dashboardRoutes.js

--- dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardStats } = require('../controllers/dashboardController');
-const { protect, requirePermission } = require('../middleware/auth');
-
-router.get('/', protect, requirePermission('viewDashboard'), getDashboardStats);
-
-module.exports = router;
