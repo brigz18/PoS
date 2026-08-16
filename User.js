8886d2d51576929baca7819df9dc346abe33bbcc
brigz18 <bringinoroiandrey@mqcqc.ph.education>
2026-08-16 16:46:49 +0800

    Update import path for PERMISSION_KEYS

--- User.js
+++ User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const { PERMISSION_KEYS } = require('permissions');
+const { PERMISSION_KEYS } = require('../utils/permissions');
 
 // Build the permissions sub-schema dynamically from PERMISSION_KEYS so this
 // file never has to be hand-edited when a new permission is added -
